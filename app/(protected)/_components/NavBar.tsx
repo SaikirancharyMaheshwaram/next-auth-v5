0a1073dad8a7b2cec9c693fa0a80b79d12589164
saikiranchary <saikiranchary136@gmail.com>
2024-03-01 23:09:07 +0530

    navbar updated

--- app/(protected)/_components/NavBar.tsx
+++ app/(protected)/_components/NavBar.tsx
@@ -1,8 +1,40 @@
 "use client";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { UserButton } from "@/components/auth/user-button";
 export const NavBar = () => {
+  const pathname = usePathname();
+
   return (
-    <nav className="bg-secondary w-[600px] items-center rounded-xl p-4 shadow-sm ">
-      Nav Bar Component
+    <nav
+      className="bg-secondary flex w-[600px] items-center justify-between
+      rounded-xl p-4 shadow-sm "
+    >
+      <div className="flex gap-x-2">
+        <Button
+          asChild
+          variant={pathname === "/settings" ? "default" : "outline"}
+        >
+          <Link href={"/settings"}>Settings</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/server" ? "default" : "outline"}
+        >
+          <Link href={"/server"}>Server</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/client" ? "default" : "outline"}
+        >
+          <Link href={"/client"}>Client</Link>
+        </Button>
+        <Button asChild variant={pathname === "/admin" ? "default" : "outline"}>
+          <Link href={"/admin"}>Admin</Link>
+        </Button>
+      </div>
+      <UserButton />
     </nav>
   );
 };
